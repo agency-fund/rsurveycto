ede63a1ab5e5790cb179b60d92763da93d0201f3
Jake Hughey <jakejhughey@gmail.com>
2022-08-05 16:02:45 -0500

    commented out test code.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,13 +1,13 @@
 library('data.table')
 library('qs')
 
-snapshot = function(xObs, path) {
-  if (file.exists(path)) {
-    xExp = qread(path)
-  } else {
-    qsave(xObs, path)
-    xExp = xObs}
-  return(xExp)}
-
-dataDir = 'data'
-if (!dir.exists(dataDir)) dir.create(dataDir)
+# snapshot = function(xObs, path) {
+#   if (file.exists(path)) {
+#     xExp = qread(path)
+#   } else {
+#     qsave(xObs, path)
+#     xExp = xObs}
+#   return(xExp)}
+#
+# dataDir = 'data'
+# if (!dir.exists(dataDir)) dir.create(dataDir)
